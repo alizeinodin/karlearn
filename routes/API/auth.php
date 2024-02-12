6bb455e4a7057f6633807b0d142488073286f1df
alizeinodin <alizeinodin79@gmail.com>
2024-02-12 22:15:54 +0330

    Fix api routes

--- routes/API/auth.php
+++ routes/API/auth.php
@@ -3,10 +3,10 @@
 use App\Http\Controllers\API\v1\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(AuthController::class)->group([
-    Route::name('auth.')->group([
-        Route::post('login', 'login')->name('login'),
-        Route::post('register', 'register')->name('login'),
-        Route::post('logout', 'logout')->name('logout')
-    ]),
-]);
+Route::controller(AuthController::class)->group(function () {
+    Route::name('auth.')->group(function () {
+        Route::post('/login', 'login')->name('login');
+        Route::post('/register', 'register')->name('login');
+        Route::post('/logout', 'logout')->name('logout');
+    });
+});
