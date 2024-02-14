246937e09e155f57c7e6dd3b3b421f4822a273fc
alizeinodin <alizeinodin79@gmail.com>
2024-02-15 00:43:46 +0330

    Add request for course controller

---
+++ app/Http/Requests/Course/StoreRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Course;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string',
+            'content' => 'string',
+            'cost' => 'integer',
+            'type' => 'in:free,combinational,unfree',
+            'index_img' => 'image|max:51200',
+            'index_video' => 'mimes:mp4,avi,mkv,webm|max:204800'
+        ];
+    }
+}
