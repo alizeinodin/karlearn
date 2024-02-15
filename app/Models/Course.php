72a55f34925d92daaaca0c9631d3ac801d9d26ea
alizeinodin <alizeinodin79@gmail.com>
2024-02-15 20:58:01 +0330

    Add one-to-many relationship to category

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -44,4 +44,9 @@ class Course extends Model
     {
         return $this->hasMany(Section::class);
     }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
