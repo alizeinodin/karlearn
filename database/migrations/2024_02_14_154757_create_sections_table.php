4033fd5ff01bd1de2847f7a3521ed7223bccdac3
alizeinodin <alizeinodin79@gmail.com>
2024-02-15 17:28:30 +0330

    Add sections table

---
+++ database/migrations/2024_02_14_154757_create_sections_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('video')->nullable(); // file address
+            $table->time('time')->nullable();
+            $table->string('resources'); // file address
+
+            $table->foreignIdFor(\App\Models\Course::class)
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sections');
+    }
+};
