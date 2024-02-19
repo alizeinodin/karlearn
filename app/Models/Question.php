56bda88e0e5d44ea65642f675cd81b0dbc962e06
alizeinodin <alizeinodin79@gmail.com>
2024-02-19 23:32:16 +0330

    Add question model and migration

---
+++ app/Models/Question.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'question_set_id',
+    ];
+
+    public function questionSet()
+    {
+        return $this->belongsTo(QuestionSet::class);
+    }
+}
