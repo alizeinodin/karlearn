1fe0c961d3014a774763a616e62c1c946aa9e4bd
alizeinodin <alizeinodin79@gmail.com>
2024-02-22 22:38:12 +0330

    Add question controller

---
+++ app/Http/Controllers/API/v1/QuestionController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Question\StoreRequest;
+use App\Http\Requests\Question\UpdateRequest;
+use App\Models\Question;
+use App\Models\QuestionSet;
+use Symfony\Component\HttpFoundation\Response;
+
+class QuestionController extends Controller
+{
+    public function store(StoreRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $questionSet = QuestionSet::create([
+            'course_id' => $validatedData['course_id'],
+        ]);
+
+        foreach ($validatedData['questions'] as $questionData) {
+            $questionSet
+                ->questions()
+                ->associate($this->makeQuestion($questionData))
+                ->save();
+        }
+
+        $response = [
+            'message' => __('questions.created'),
+            'questions' => $questionSet
+        ];
+
+        return jsonResponse($response, Response::HTTP_CREATED);
+    }
+
+    public function show(Question $question)
+    {
+        return jsonResponse($question, Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRequest $request, Question $question)
+    {
+        $validatedData = $request->validated();
+
+        $question->update($validatedData);
+        $question->refresh();
+
+        $response = [
+            'message' => __('question.updated'),
+            'question' => $question
+        ];
+
+        return jsonResponse($response, Response::HTTP_OK);
+
+    }
+
+    public function destroy(Question $question)
+    {
+        $question->delete();
+
+        $response = [
+            'message' => __('question.deleted'),
+        ];
+
+        return jsonResponse($response, Response::HTTP_NO_CONTENT);
+    }
+
+    protected function makeQuestion(array $data): Question
+    {
+        $question = new Question();
+        $question->title = $data['title'];
+        return $question;
+    }
+}
