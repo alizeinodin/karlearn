730614f7475a9fa78f9140b717684e9d0e77046b
alizeinodin <alizeinodin79@gmail.com>
2024-02-22 22:43:51 +0330

    Add requests for quizzes

---
+++ app/Http/Requests/Quiz/StoreRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Quiz;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'string',
+            'description' => 'string',
+            'time' => 'required|date_format:H:i:s',
+            'constraint_time' => 'required|date_format:H:i:s',
+            'course_id' => 'required|exists:courses,id',
+        ];
+    }
+}
