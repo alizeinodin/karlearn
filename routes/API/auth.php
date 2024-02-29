974ad9fa90d26e5e34382007bbea80b9ba3a374c
alizeinodin <alizeinodin79@gmail.com>
2024-02-29 23:33:21 +0330

    Add sanctum middleware to logout route

--- routes/API/auth.php
+++ routes/API/auth.php
@@ -7,6 +7,8 @@ Route::controller(AuthController::class)->group(function () {
     Route::name('auth.')->group(function () {
         Route::post('/login', 'login')->name('login');
         Route::post('/register', 'register')->name('login');
-        Route::post('/logout', 'logout')->name('logout');
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::post('/logout', 'logout')->name('logout');
+        });
     });
 });
