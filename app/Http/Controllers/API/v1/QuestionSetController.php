95abf4fc93db135e670a7325d7829007bb93bbf3
alizeinodin <alizeinodin79@gmail.com>
2024-03-12 11:44:40 +0330

    Add update method and random questionSet

--- app/Http/Controllers/API/v1/QuestionSetController.php
+++ app/Http/Controllers/API/v1/QuestionSetController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\QuestionSet\UpdateRequest;
 use App\Models\Course;
 use App\Models\QuestionSet;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,12 +12,28 @@ class QuestionSetController extends Controller
 {
     public function get(Course $course)
     {
-        return jsonResponse($course->load('questionSets'), Response::HTTP_OK);
+        $questionSet = $course->questionSets()->inRandomOrder()->first();
+        return jsonResponse($questionSet, Response::HTTP_OK);
     }
 
     public function show(QuestionSet $questionSet)
     {
         return jsonResponse($questionSet, Response::HTTP_OK);
+    }
+
+    public function update(UpdateRequest $request, QuestionSet $questionSet)
+    {
+        $validatedData = $request->validated();
+
+        $questionSet->update($validatedData);
+        $questionSet->refresh();
+
+        $message = [
+            'message' => __('questionSets.updated'),
+            'questionSet' => $questionSet,
+        ];
+
+        return jsonResponse($message, Response::HTTP_OK);
     }
 
     public function destroy(QuestionSet $questionSet)
